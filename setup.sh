f5d82f6a7c981aa2629b4b3ee7347027caf23e80
Wade Beveridge <wadebev11@gmail.com>
2025-08-05 20:05:06 -0600

    Add fav git aliases and user setup

--- setup.sh
+++ setup.sh
@@ -5,6 +5,13 @@ set -e
 git config --global push.autoSetupRemote True
 git config --global remote.origin.prune True
 git config --global core.editor "vim"
+git config --global user.name "Wade Beveridge"
+git config --global user.email wadebev11@gmail.com
+
+git config --global alias.branch-name "!git rev-parse --abbrev-ref HEAD"
+git config --global alias.st "status"
+git config --global alias.originreset "!git fetch; git reset --hard origin/$(git branch-name)"
+git config --global alias.origindiff "!git fetch; git diff origin/$(git branch-name)"
 
 echo "git configuration finished"
 
